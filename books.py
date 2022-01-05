ba7883a11bae4164f9f1dcdec1e964b1eccf8b5c
Sargis <sargis.ab@gmail.com>
2022-01-05 13:11:59 -0600

    add ratings

--- books.py
+++ books.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import json
-
+import csv
 
 def getHTML(url):
     response = requests.get(url)
@@ -13,16 +13,27 @@ book_data = []
 for x in range(1,51):
     html = getHTML(f"http://books.toscrape.com/catalogue/page-{x}.html")
     soup = BeautifulSoup(html, 'html.parser')
-    rows = soup.find_all('article', attrs = {'class':'product_pod'})
-    for row in rows:
+    articles = soup.find_all('article', attrs = {'class':'product_pod'})
 
-        title = row.find('h3').find('a').attrs['title']
-        price = row.find('div', attrs = {'class':'product_price'}).find('p', attrs = {'class':'price_color'}).text.strip("Â£")
-        
-        book_data.append({'title': title, 'price': price})
+    for article in articles:
+
+        title = article.find('h3').find('a').attrs['title']
+        price = article.find('div', attrs = {'class':'product_price'}).find('p', attrs = {'class':'price_color'}).text.strip("Â£")
+        rating = ''.join(article.find('p')['class']).strip()[11:]
+
+        book_data.append({'title': title, 'price': price, 'rating': rating})
 
 
 books = json.dumps(book_data)
 
 with open('books.json', 'w') as j_file:
     j_file.write(books)
+
+
+# keys = book_data[0].keys()
+
+# with open('books.csv', 'w') as csv_file:
+#     dict_writer = csv.DictWriter(csv_file, keys)
+#     dict_writer.writeheader()
+#     dict_writer.writerows(book_data)
+
